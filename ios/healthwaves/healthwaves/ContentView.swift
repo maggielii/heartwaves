7f451386638cd464f4bb814bf8e8bd737723b016
Sophia Xu <hrxu@@students.cs.ubc.ca>
2026-02-07 17:24:32 -0800

    added healthkit data

--- ios/healthwaves/healthwaves/ContentView.swift
+++ ios/healthwaves/healthwaves/ContentView.swift
@@ -1,15 +1,22 @@
 import SwiftUI
+import HealthKit
 
 struct ContentView: View {
     private let hk = HealthKitManager()
 
     @State private var status = "Not connected"
     @State private var stepsText = "--"
+    @State private var distanceText = "--"
+    @State private var energyText = "--"
 
     var body: some View {
         VStack(spacing: 16) {
             Text(status)
             Text("Today’s steps: \(stepsText)")
+                .font(.title2)
+            Text("Today’s distance: \(distanceText)")
+                .font(.title2)
+            Text("Today’s acgive energy: \(energyText)")
                 .font(.title2)
 
             Button("Connect Health") {
@@ -17,19 +24,22 @@ struct ContentView: View {
                     status = success ? "✅ Authorized" : "❌ Not authorized: \(message)"
                     if success {
                         loadSteps()
+                        loadDistance()
+                        loadActiveEnergy()
                     }
                 }
             }
-
             Button("Refresh Steps") {
                 loadSteps()
+                loadDistance()
+                loadActiveEnergy()
             }
         }
         .padding()
     }
 
     private func loadSteps() {
-        hk.fetchTodaySteps { steps, error in
+        hk.fetchTodayGeneral(id: .stepCount, unit: .count(), completion: { steps, error in
             if let error = error {
                 status = "❌ Step fetch failed: \(error)"
                 stepsText = "--"
@@ -37,6 +47,36 @@ struct ContentView: View {
                 status = "✅ Steps loaded"
                 stepsText = String(Int(steps))
             }
+        
         }
+        )
+    }
+    
+    private func loadDistance() {
+        hk.fetchTodayGeneral(id: .distanceWalkingRunning, unit: .meter(), completion: { distance, error in
+            if let error = error {
+                status = "❌ Step fetch failed: \(error)"
+                distanceText = "--"
+            } else {
+                status = "✅ Steps loaded"
+                distanceText = String(Int(distance))
+            }
+        
+        }
+        )
+    }
+    
+    private func loadActiveEnergy() {
+        hk.fetchTodayGeneral(id: .activeEnergyBurned, unit: .kilocalorie(), completion: { energy, error in
+            if let error = error {
+                status = "❌ Step fetch failed: \(error)"
+                energyText = "--"
+            } else {
+                status = "✅ Steps loaded"
+                energyText = String(Int(energy))
+            }
+        
+        }
+        )
     }
 }
--- ios/healthwaves/healthwaves/HealthKitManager.swift
+++ ios/healthwaves/healthwaves/HealthKitManager.swift
@@ -13,27 +13,35 @@ final class HealthKitManager {
             completion(false, "StepCount type not available")
             return
         }
+        guard let distance = HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning) else {
+            completion(false, "Distance type not available")
+            return
+        }
+        guard let activeenergy = HKObjectType.quantityType(forIdentifier: .activeEnergyBurned) else {
+            completion(false, "activeEnergyBurned type not available")
+            return
+        }
 
-        store.requestAuthorization(toShare: [], read: [steps]) { success, error in
+        store.requestAuthorization(toShare: [], read: [steps, distance, activeenergy]) { success, error in
             DispatchQueue.main.async {
                 completion(success, error?.localizedDescription ?? "OK")
             }
         }
     }
 
-    func fetchTodaySteps(completion: @escaping (Double, String?) -> Void) {
-        guard let stepsType = HKQuantityType.quantityType(forIdentifier: .stepCount) else {
-            completion(0, "StepCount type not available")
+    func fetchTodayGeneral(id: HKQuantityTypeIdentifier, unit: HKUnit, completion: @escaping (Double, String?) -> Void) {
+        guard let fetchType = HKQuantityType.quantityType(forIdentifier: id) else {
+            completion(0, "Type not available")
             return
         }
 
         let startOfDay = Calendar.current.startOfDay(for: Date())
         let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: Date(), options: .strictStartDate)
 
-        let query = HKStatisticsQuery(quantityType: stepsType,
+        let query = HKStatisticsQuery(quantityType: fetchType,
                                       quantitySamplePredicate: predicate,
                                       options: .cumulativeSum) { _, result, error in
-            let sum = result?.sumQuantity()?.doubleValue(for: .count()) ?? 0
+            let sum = result?.sumQuantity()?.doubleValue(for: unit) ?? 0
             DispatchQueue.main.async {
                 completion(sum, error?.localizedDescription)
             }
